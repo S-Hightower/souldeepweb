ea22d57df2e9e60a51ffc2e3bcb8e5b7adb5eb1d
Shawna Hightower <sdhightower6@gmail.com>
2022-01-26 18:03:20 -0700

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,39 +6,37 @@ import styles from './component_css/Header.module.css';
 function Header() {
 
     return(
-        <div className={styles.container}>
-            <div className={styles.header}>
-            <div className={styles.links}>
-                <Link to={'/'}>
-                    <h1>Soul Deep Life</h1>
-                </Link>
-                <Link to={'/about'}>
-                    <p>About</p>
-                </Link>
-                <Link to={'/reiki'}>
-                    <p>Reiki</p>
-                </Link>
-                <Link to={'/pastlife'}>
-                    <p>Past Life Readings & Clearings</p>
-                </Link>
-                <Link to={'/energyclearing'}>
-                    <p>Energetic Emotion Body Clearing</p>
-                </Link>
-                <Link to={'/soulcoaching'}>
-                    <p>Soul Coaching</p>
-                </Link>
-                <Link to={'/testimonials'}>
-                    <p>Testimonials</p>
-                </Link>
-                <Link to={'/contact'}>
-                    <p>Contact</p>
-                </Link>
-                <Link to={'/resources'}>
-                    <p>Resources</p>
-                </Link>
-            </div>
-            </div>{/* header */}
-        </div>//container
+        <div className={styles.header}>
+        <div className={styles.links}>
+            <Link to={'/'}>
+                <h1>Soul Deep Life</h1>
+            </Link>
+            <Link to={'/about'}>
+                <p>About</p>
+            </Link>
+            <Link to={'/reiki'}>
+                <p>Reiki</p>
+            </Link>
+            <Link to={'/pastlife'}>
+                <p>Past Life Readings & Clearings</p>
+            </Link>
+            <Link to={'/energyclearing'}>
+                <p>Energetic Emotion Body Clearing</p>
+            </Link>
+            <Link to={'/soulcoaching'}>
+                <p>Soul Coaching</p>
+            </Link>
+            <Link to={'/testimonials'}>
+                <p>Testimonials</p>
+            </Link>
+            <Link to={'/contact'}>
+                <p>Contact</p>
+            </Link>
+            <Link to={'/resources'}>
+                <p>Resources</p>
+            </Link>
+        </div>
+        </div>
     );
 }
 
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
+import styles from "./view_css/Main.module.css"
 
 function Main() {
     return(
-        <Header />
+        <div className={styles.container}>
+            <Header />
+            <Footer />
+        </div>
     )
 }
 
