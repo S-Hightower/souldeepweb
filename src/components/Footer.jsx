8b0ebbec9662e85103fc49e913d69831724ff576
Shawna Hightower <sdhightower6@gmail.com>
2022-01-27 12:48:29 -0700

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import styles from './component_css/Footer.module.css';
 
 function Footer() {
 
     return(
-        <div className={styles.container}>
+        <div className={styles.footer}>
             <div className={styles.footer_start}>
                 <h1>We Would Love For You to Visit Soon!</h1>
-            </div>
-            <div className={styles.socials}>
-                <a href="https://www.facebook.com/SoulDeepLife/?hc_ref=ARQksql18gBASM8xkYt608htSsVHXoAK34ohihxtaQWDznq4eoGf1NiDN5Y8_U7ZzzE&ref=nf_target&__tn__=kC-R" class="fa fa-facebook"></a>
-                <a href="https://www.linkedin.com/in/brenda-martinez-zen1/linkedin.com" class="fa fa-linkedin"></a>
-                <a href="https://www.instagram.com/brenda.martinez.psychic/" class="fa fa-instagram"></a>
             </div>
             <div>
                 <h2>Hours:</h2>
@@ -21,7 +15,12 @@ function Footer() {
                 <h2>Email:</h2>
                 <h4>souldeeplife@gmail.com</h4>
             </div>
-        </div>//container
+            <div className={styles.socials}>
+                <a href="https://www.facebook.com/SoulDeepLife/?hc_ref=ARQksql18gBASM8xkYt608htSsVHXoAK34ohihxtaQWDznq4eoGf1NiDN5Y8_U7ZzzE&ref=nf_target&__tn__=kC-R" class="fa fa-facebook"></a>
+                <a href="https://www.linkedin.com/in/brenda-martinez-zen1/linkedin.com" class="fa fa-linkedin"></a>
+                <a href="https://www.instagram.com/brenda.martinez.psychic/" class="fa fa-instagram"></a>
+            </div>
+        </div>
     );
 }
 
