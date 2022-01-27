76402344445cb94e9bd817c0c5e6fe05f3803075
Shawna Hightower <sdhightower6@gmail.com>
2022-01-27 13:33:58 -0700

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { SocialIcon } from 'react-social-icons';
+
 import styles from './component_css/Footer.module.css';
 
 function Footer() {
@@ -8,17 +10,17 @@ function Footer() {
         <div className={styles.footer}>
             <div className={styles.footer_start}>
                 <h1>We Would Love For You to Visit Soon!</h1>
-            </div>
             <div>
+                <SocialIcon url="https://www.facebook.com/SoulDeepLife/?hc_ref=ARQksql18gBASM8xkYt608htSsVHXoAK34ohihxtaQWDznq4eoGf1NiDN5Y8_U7ZzzE&ref=nf_target&__tn__=kC-R" style={{ height: 25, width: 25, marginRight: 5 }}/>
+                <SocialIcon url="https://www.linkedin.com/in/brenda-martinez-zen1/linkedin.com" style={{ height: 25, width: 25, marginRight: 5 }}/>
+                <SocialIcon url="https://www.instagram.com/brenda.martinez.psychic/" style={{ height: 25, width: 25, marginRight: 5}}/>
+            </div>
+            </div>
+            <div className={styles.footer_info}>
                 <h2>Hours:</h2>
                 <h4>M-F: 9am - 5pm</h4>
                 <h2>Email:</h2>
                 <h4>souldeeplife@gmail.com</h4>
-            </div>
-            <div className={styles.socials}>
-                <a href="https://www.facebook.com/SoulDeepLife/?hc_ref=ARQksql18gBASM8xkYt608htSsVHXoAK34ohihxtaQWDznq4eoGf1NiDN5Y8_U7ZzzE&ref=nf_target&__tn__=kC-R" class="fa fa-facebook"></a>
-                <a href="https://www.linkedin.com/in/brenda-martinez-zen1/linkedin.com" class="fa fa-linkedin"></a>
-                <a href="https://www.instagram.com/brenda.martinez.psychic/" class="fa fa-instagram"></a>
             </div>
         </div>
     );
