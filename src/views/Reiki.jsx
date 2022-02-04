49b5e5eb9f5b1ca099f41ad2bf1ede321e1efa92
Shawna Hightower <sdhightower6@gmail.com>
2022-02-04 09:27:37 -0700

    reiki page updates

--- src/views/Reiki.jsx
+++ src/views/Reiki.jsx
@@ -29,7 +29,7 @@ const Reiki = (props) => {
                 <h1>I am a Reiki Master Teacher and Intuitive who assists you in healing yourself</h1>
             </div>
 
-                <h4>Please select if you are interested in Reiki services or classes:</h4>
+                <h4>Please select whether you are interested in Reiki sessions or classes:</h4>
             <div className={styles.clickers}>
                 <div className="btn-group btn-group-lg mb-5" role="group">
                     <button className="btn border border-dark" onClick={e => onClickHandler(e,1)} value={selection}>Sessions</button>
@@ -41,20 +41,26 @@ const Reiki = (props) => {
                 { !wasClicked }
                 {selection === 1 && 
                     <div className={styles.session}>
-                        <h2>Reiki Session</h2>
-                        <h4>In person, lasting about 1 to 1 1/2 hours.</h4>
-                        <h5>I like to block 2 hours for this session, so that we have time to talk beforehand, and afterwards. The session can go from an hour to an hour and a half depending on the needs of the client.</h5>
-                        <h3>Add a mini card reading for $10 (in person at session)</h3>
+                        <div className="card text-start bg-light p-2 text-dark bg-opacity-10" style={{width: 700}}>
+                            <h1>Reiki Session</h1>
+                            <h4>🔹in person, lasting about 1 to 1 1/2 hours.</h4>
+                            <h5>I like to block 2 hours for this session, so that we have time to talk beforehand, and afterwards. The session can go from an hour to an hour and a half depending on the needs of the client.</h5>
+                            <h4>Add a mini card reading for $10 (in person at session)</h4>
+                        </div>
                     <br />
-                        <h2>Long Distance Reiki Session</h2>
-                        <h4>1/2 hour</h4>
-                        <h5>For those times when you can't make it to me, or if you are out of town, state or country and would like a session. This session is about 30 minutes as I can work remotely on both sides of your body at the same time - we then speak on the phone afterwards to go over any findings or issues that showed up during the session. </h5>
+                        <div className="card text-start bg-light p-2 text-dark bg-opacity-10" style={{width: 700}}>
+                            <h1>Long Distance Reiki Session</h1>
+                            <h4>🔹1/2 hour</h4>
+                            <h5>For those times when you can't make it to me, or if you are out of town, state or country and would like a session. This session is about 30 minutes as I can work remotely on both sides of your body at the same time - we then speak on the phone afterwards to go over any findings or issues that showed up during the session. </h5>
+                        </div>
                     <br />
-                        <h2>Pet Reiki</h2>
-                        <h4>local or distant</h4>
-                        <h5>Our fur babies sometimes need a little extra love and attention. Reiki can help with anxiety & aches and hurts and just about anything that it can help a human with.</h5>
+                        <div className="card text-start bg-light p-2 text-dark bg-opacity-10" style={{width: 700}}>
+                            <h1>Pet Reiki</h1>
+                            <h4>🔹local or distant</h4>
+                            <h5>Our fur babies sometimes need a little extra love and attention. Reiki can help with anxiety & aches and hurts and just about anything that it can help a human with.</h5>
+                        </div>
                     <br />
-                        <div className="text-danger mb-3">
+                        <div className="card text-start bg-light p-2 bg-opacity-25 text-danger" style={{width: 900}}>
                             <h4>All Cancellations with less than 24 hours notice may be charged Full session price.</h4>
                         </div>
                     </div>
@@ -73,22 +79,29 @@ const Reiki = (props) => {
                         <h5><span>Private Classes add $25</span> -  **Turn your private class in to a group class by bringing a friend to save $25</h5>
 
                     <br />
-                        <h2>Reiki I Class</h2>
-                        <h5>Reiki 1 includes your first attunement, Reiki 1 manual - included in the manual is a history of Reiki, a meditation to meet your Reiki guide, information on your Chakras, Reiki etiquette, instructions on how to have a Reiki practice, and much more. The classes are small and very hands-on, meaning you will receive personalized instruction on the basics of doing a full Reiki session, so as to be comfortable to do sessions on your own. You will be working directly with myself and/or other participants in the class. Upon completion of the class you will receive your First Degree Reiki Certification. Also included with this class, as well as Reiki 2, Reiki 3a, and Master Teacher classes, is an ongoing opportunity to come back at any time to join a scheduled class to refresh, or gain additional knowledge for your own practice. The only prerequisite is that you have taken that degree/class prior to coming back, and that there is room for you in the class. </h5>
-
+                    <div className="card text-start bg-light p-2 text-dark bg-opacity-10" style={{width: 900}}>
+                        <h1>Reiki I Class</h1>
+                        <h5>📍 includes your first attunement and Reiki 1 manual📍</h5>
+                        <h5>included in the manual is a history of Reiki, a meditation to meet your Reiki guide, information on your Chakras, Reiki etiquette, instructions on how to have a Reiki practice, and much more. The classes are small and very hands-on, meaning you will receive personalized instruction on the basics of doing a full Reiki session, so as to be comfortable to do sessions on your own. You will be working directly with myself and/or other participants in the class. Upon completion of the class you will receive your First Degree Reiki Certification. Also included with this class, as well as Reiki 2, Reiki 3a, and Master Teacher classes, is an ongoing opportunity to come back at any time to join a scheduled class to refresh, or gain additional knowledge for your own practice. The only prerequisite is that you have taken that degree/class prior to coming back, and that there is room for you in the class. </h5>
+                    </div>
                     <br />
-                        <h2>Reiki II Class</h2>
-                        <h5>Reiki 2 includes your second attunement to include 3 Reiki symbols, the Reiki 2 manual, including the use of the three Reiki symbols to include distant Reiki, instructions on how the symbols work, and much more. The class is hands on, meaning you will receive personalized instruction from myself, working with myself directly and/or other participants in the class.  We will learn how to use the symbols by practicing using them. We will also go over the heightened intuition Reiki 2 can bring, and more... Upon completion of the class you will receive your Second Degree Reiki Certification.</h5>
-
+                    <div className="card text-start bg-light p-2 text-dark bg-opacity-10" style={{width: 900}}>
+                        <h1>Reiki II Class</h1>
+                        <h5>📍includes your second attunement to include 3 Reiki symbols and the Reiki 2 manual📍</h5>
+                        <h5>including the use of the three Reiki symbols to include distant Reiki, instructions on how the symbols work, and much more. The class is hands on, meaning you will receive personalized instruction from myself, working with myself directly and/or other participants in the class.  We will learn how to use the symbols by practicing using them. We will also go over the heightened intuition Reiki 2 can bring, and more... Upon completion of the class you will receive your Second Degree Reiki Certification.</h5>
+                    </div>
                     <br />
-                        <h2>Reiki Expert Class</h2>
+                    <div className="card text-start bg-light p-2 text-dark bg-opacity-10" style={{width: 900}}>
+                        <h1>Reiki Expert Class</h1>
                         <h5>This is an advanced class, and is the prerequisite to your Master Teachers Certification. In this class you will be attuned to the the Master symbol without the ability to pass attunements.  It is for those who know they want to take their practice to another level, and is a good step to decide if you want to go on to teach Reiki. In this class you will receive your Expert level manual, and will receive hands on training. </h5>
                         <h5>Upon completion of the class you will receive the Expert Level Certification.</h5>
                         <h4>(to be eligible to take the Master Teacher course, you must complete 100 hours of Reiki practice)</h4>
-
+                    </div>
                     <br />
-                        <h2>Reiki Master Teacher Class including one time help with your first class</h2>
+                    <div className="card text-start bg-light p-2 text-dark bg-opacity-10" style={{width: 900}}>
+                        <h1>Reiki Master Teacher Class including one time help with your first class</h1>
                         <h5>This class will give you the tools to become a competent White Light Reiki Master Teacher. The requirements for this course are the study, certification and completion of Reiki 1, 2, & 3a, along with a documented 100 hours of reiki practitioner practice. This class will give you all of the tools needed to pass attunements as well as all of the written material needed to create your own manuals and certificates, and of course me to help with your very first class. </h5>
+                    </div>
                 </div>
                 }
             </div>
