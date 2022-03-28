06a857c8109042fa128c17b1b507a37204e6ed46
Shawna Hightower <sdhightower6@gmail.com>
2022-03-28 13:19:29 -0600

    updates to dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import { ReactComponent as WaveIcon } from './component_css/icons/wave.svg';
 
 function Header() {
 
-    return(
+    return (
         <Navbar>
             <NavItem icon={< WaveIcon />}>
                 <DropdownMenu></DropdownMenu>
@@ -26,44 +26,104 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
-    const [ open, setOpen ] = useState(false);
+    const [open, setOpen] = useState(false);
 
     return (
+        <li className="nav-item">
+            <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+                {props.icon}
+            </a>
 
+            {open && props.children}
+        </li>
     );
 }
 
-    return(
-        <div className={styles.header}>
-        <div className={styles.links}>
-            <Link to={'/'}>
-                <h1>Soul Deep Life</h1>
-            </Link>
-            <Link to={'/about'}>
-                <p>About</p>
-            </Link>
-            <Link to={'/testimonials'}>
-                <p>Testimonials</p>
-            </Link>
-            <Link to={'/contact'}>
-                <p>Contact</p>
-            </Link>
-            <Link to={'/resources'}>
-                <p>Resources</p>
-            </Link>
-            <Link to={'/reiki'}>
-                <p>Reiki</p>
-            </Link>
-            <Link to={'/pastlife'}>
-                <p>Past Life Readings & Clearings</p>
-            </Link>
-            <Link to={'/energyclearing'}>
-                <p>Energetic Emotion Body Clearing</p>
-            </Link>
-            <Link to={'/soulcoaching'}>
-                <p>Soul Coaching</p>
-            </Link>
-        </div>
+function DropdownMenu() {
+    const [activeMenu, setActiveMenu] = useState('main');
+    const [menuHeight, setMenuHeight] = useState(null);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+    }, [])
+
+    function calcHeight(el) {
+        const height = el.offsetHeight;
+        setMenuHeight(height);
+    }
+
+    function DropdownItem(props) {
+        return (
+            <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+                <span className="icon-button">{props.leftIcon}</span>
+                {props.children}
+                <span className="icon-right">{props.rightIcon}</span>
+            </a>
+        );
+    }
+
+
+    return (
+        <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+
+            <CSSTransition
+                in={activeMenu === 'main'}
+                timeout={500}
+                classNames="menu-primary"
+                unmountOnExit
+                onEnter={calcHeight}>
+                <div className="menu">
+                    <DropdownItem>My Profile</DropdownItem>
+                    <DropdownItem
+                        leftIcon={<CogIcon />}
+                        rightIcon={<ChevronIcon />}
+                        goToMenu="settings">
+                        Settings
+                    </DropdownItem>
+                    <DropdownItem
+                        leftIcon="🦧"
+                        rightIcon={<ChevronIcon />}
+                        goToMenu="animals">
+                        Animals
+                    </DropdownItem>
+
+                </div>
+            </CSSTransition>
+
+            <CSSTransition
+                in={activeMenu === 'settings'}
+                timeout={500}
+                classNames="menu-secondary"
+                unmountOnExit
+                onEnter={calcHeight}>
+                <div className="menu">
+                    <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+                        <h2>My Tutorial</h2>
+                    </DropdownItem>
+                    <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
+                    <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
+                    <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
+                    <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
+                </div>
+            </CSSTransition>
+
+            <CSSTransition
+                in={activeMenu === 'animals'}
+                timeout={500}
+                classNames="menu-secondary"
+                unmountOnExit
+                onEnter={calcHeight}>
+                <div className="menu">
+                    <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+                        <h2>Animals</h2>
+                    </DropdownItem>
+                    <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+                    <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+                    <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+                    <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+                </div>
+            </CSSTransition>
         </div>
     );
 }
