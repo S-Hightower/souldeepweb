4c37597cd2c45248bbad9e1b35ee23360991dc6a
Shawna Hightower <sdhightower6@gmail.com>
2022-04-18 14:51:41 -0600

    background update test1

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -27,7 +27,9 @@ function About() {
                 <br />
             I believe in putting love into all that I do.
                 <br />
-            Happy - Healthy- Whole - ​This is my desire for you.</h2>
+            <span>Happy - Healthy - Whole
+                <br />
+            ​This is my desire for you.</span></h2>
         </div>
 
         <div className={styles.about_body}>
@@ -60,9 +62,9 @@ function About() {
             </div>
 
             <div className={styles.wrapper}>
-                <h4>With both of these modalities under my belt, and after years of learning to trust that still inner voice, as well as the amazing pictures that reveal themselves to me I am now fully out of the closet as a Psychic. My clear knowing is and has been my strongest Clair, but coming in close second is Clairvoyance. I use these gifts throughout my practice in Reiki, Akashic Readings, Emotion Body therapy, Astrology and through my Card Readings; & in many cases it comes through just simply speaking with you. </h4>
+                <h4>With both of these modalities under my belt, and after years of learning to trust that still inner voice, as well as the amazing pictures that reveal themselves to me, I am now fully out of the closet as a Psychic.</h4>
                 <img src={outlook} width="500" height="500" alt="scenic outlook towards the ocean and horizon" className="me-5 ms-5"/>
-                <h4>I'm learning and growing everyday and it is my mission to help you discover your gifts as well.</h4>
+                <h4>Claircognizance is and has been my strongest gift, but coming in close second is Clairvoyance. I use them throughout my practice in Reiki, Akashic Readings, Emotional Body Release, Astrology, and my Card Readings. In many cases it comes through just simply speaking with you. </h4>
             </div>
             
             <div className="card bg-light p-2 text-dark bg-opacity-25 mt-5" >
